82eb31d8d7f5b328540de9dc9b041adfd7db80e4
Yury Antonov <yantonov@yandex.ru>
2016-08-30 07:22:57 +0500

    action creator does not depends on dispatch fn

--- src/rex/ext/action_creator.cljs
+++ src/rex/ext/action_creator.cljs
@@ -5,9 +5,8 @@
   [action get-store next-dispatch-fn]
   (letfn [(action-creator-dispatch-fn [action]
             (if (fn? action)
-              (let [action-creator action]
-                (do
-                  (action-creator action-creator-dispatch-fn get-store)
-                  (get-store)))
+              (do
+                (action get-store)
+                (get-store))
               (next-dispatch-fn action)))]
     (action-creator-dispatch-fn action)))
